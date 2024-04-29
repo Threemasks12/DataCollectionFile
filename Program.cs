0883c1a6c2bc4a029927e552711d6db67d430f60
Threemasks12 <jongeorgmagnussen@gmail.com>
2024-04-29 15:33:22 +0200

    starting program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+class writer
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader sr = new StreamReader(args[0]))
+        {
+            String line = sr.ReadToEnd();
+            Console.WriteLine(line);
+        }
+    }
+}
